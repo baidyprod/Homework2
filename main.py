36c6f078d14022fcd8075eb52ebf5d9a53319d4b
baidyprod <dima220920041@gmail.com>
2022-10-20 10:31:11 +0300

    V 2.0

--- main.py
+++ main.py
@@ -2,31 +2,28 @@ a=int(input("Сторона а = "))
 b=int(input("Сторона b = "))
 c=int(input("Сторона c = "))
 
-perimeter = a + b + c
-
-
-
-if a > b and a > c:
-    max = a
-elif b > a and b > c:
-    max = b
-else:
-    max = c
-
-if a < b and a < c:
-    min = a
-elif b < a and b < c:
-    min = b
-else:
-    min = c
-
-
-
 if a == 0 or b == 0 or c == 0:
     print("Таких трикутників не існує")
-elif perimeter < 10:
-    print(min)
-elif perimeter > 20:
-    print(max)
 else:
-    print("Периметр = ", perimeter)
+    perimeter = a + b + c
+
+    if a > b and a > c:
+        max = a
+    elif b > a and b > c:
+        max = b
+    else:
+        max = c
+
+    if a < b and a < c:
+        min = a
+    elif b < a and b < c:
+        min = b
+    else:
+        min = c
+
+    if perimeter < 10:
+        print(min)
+    elif perimeter > 20:
+        print(max)
+    else:
+        print("Периметр = ", perimeter)
